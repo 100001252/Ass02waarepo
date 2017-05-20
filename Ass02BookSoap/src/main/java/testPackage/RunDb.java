6b937c13df0eaac81f68d7a13f1d2a272fd0190d
mansour.sh <100001252@student.swin.edu.au>
2017-05-20 17:00:09 +1000

    some changes in test class for book and student

--- Ass02BookSoap/src/main/java/testPackage/RunDb.java
+++ Ass02BookSoap/src/main/java/testPackage/RunDb.java
@@ -1,180 +1,152 @@
 package testPackage;
-import au.edu.swin.waa.*;
+
+import java.sql.ResultSet;
+
+import au.edu.swin.waa.Ass02BookSoap;
+import au.edu.swin.waa.Book;
+import au.edu.swin.waa.Db;
 import au.edu.swin.waa.Db.DbStudentBorrow;
 
-import java.sql.*;
-
-
 public class RunDb {
-	
-public static void main(String[] args){// ToDO
-	try{
-					//	testReadBook();
-					//	testReadStudent();
-					// 	testinsertBook();
-					//testgetLastid();
-					//	testdeleteBook();
-		//testUniLibgetAllBook();
-		//testUniLibgetStudentRecord();
-		//testUniLibgetStudentRecord2();
-		//testUniLibgetspecificBook();
-	//	testUniLibupdateBook("my title", "0000000012");
-		
-				//		testUniLibaddBook("tet", "1234444446");
-		testInsertIntoborrow();
-						
-						
-					        
-	}catch(Exception e)
-	{
-						System.out.println(e.getMessage());
-	}
+
+	public static void main(String[] args) {// ToDO
+		try {
+			// testReadBook();
+			// testReadStudent();
+			// testinsertBook();
+			// testgetLastid();
+			// testdeleteBook();
+			testUniLibgetAllBook();
+			// testUniLibgetStudentRecord();
+			// testUniLibgetStudentRecord2();
+			// testUniLibgetspecificBook();
+			// testUniLibupdateBook("my title", "0000000012");
+
+			// testUniLibaddBook("tet", "1234444446");
+			// testInsertIntoborrow();
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
-public static void testInsertIntoborrow(){
-	try{
-		DbStudentBorrow dbbrw=new Db().new DbStudentBorrow();
-		dbbrw.insertRecords(2, 2, "borrow");
-		
-		
-		
-		}catch(Exception e)
-		{
+	public static void testInsertIntoborrow() {
+		try {
+			DbStudentBorrow dbbrw = new Db().new DbStudentBorrow();
+			dbbrw.insertRecords(2, 2, "borrow");
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
-	
-}
-
-public static void testUniLibaddBook(String title,String isbn){
-	try{
-	Ass02BookSoap ubook=new Ass02BookSoap();
-	ubook.addBook(title, "authortest", isbn, "testPublisher", "22/08/1980", "available");
-	}catch(Exception e)
-	{
-		System.out.println(e.getMessage());
 	}
-			
-}
 
+	public static void testUniLibaddBook(String title, String isbn) {
+		try {
+			Ass02BookSoap ubook = new Ass02BookSoap();
+			ubook.addBook(title, "authortest", isbn, "testPublisher",
+					"22/08/1980", "available");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
-public static void testUniLibupdateBook(String title,String isbn){
-	try{
-	Ass02BookSoap ubook=new Ass02BookSoap();
-	ubook.updateBook(1, title, "", isbn, "nothin","publisher", "on loan");
-	}catch(Exception e)
-	{
-		System.out.println(e.getMessage());
 	}
-			
-}
 
-public static void testUniLibgetspecificBook(){
-	Ass02BookSoap ubook=new Ass02BookSoap();
-	ubook.getBook(2);
-			
-}
-public static void testUniLibgetStudentRecord2(){
-	Ass02BookSoap ubook=new Ass02BookSoap();
-	ubook.getRecordsStudent2(1);
-			
-}
+	public static void testUniLibupdateBook(String title, String isbn) {
+		try {
+			Ass02BookSoap ubook = new Ass02BookSoap();
+			ubook.updateBook(1, title, "", isbn, "nothin", "publisher",
+					"on loan");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
-public static void testUniLibgetStudentRecord(){
-	Ass02BookSoap ubook=new Ass02BookSoap();
-	ubook.getRecordsStudent(1);
-			
-}
-
-public static void testUniLibgetAllBook(){
-	Ass02BookSoap ubook=new Ass02BookSoap();
-	ubook.getAllBook();
-			
-}
-
-
-
-
-public static void testgetLastid()
-{
-	try{
-		
-	   Db.DbBook dbBook=new Db("").new DbBook();
-	   Integer bookid=dbBook.getLastIdinserted();
-	  
-	   
-	   System.out.println(bookid);
-       
-        
-	}catch(Exception e)
-	{
-		System.out.println("11122");
-		e.printStackTrace();
 	}
-	
-}
 
-public static void testdeleteBook()
-{
-	try{
-		System.out.println("##### delete last element#####");
-	   Db.DbBook dbBook=new Db("").new DbBook();
-	   
-               
-	   
-	   Integer bookid=dbBook.getLastIdinserted();
-	   dbBook.deleteBook(bookid);
-       
-        
-	}catch(Exception e)
-	{
-		System.out.println("11122");
-		e.printStackTrace();
-	}
-	
-}
+	public static void testUniLibgetspecificBook() {
+		Ass02BookSoap ubook = new Ass02BookSoap();
+		ubook.getBook(2);
 
-public static void testinsertBook()
-{
-	try{
-		System.out.println("##### insert book#####");
-	   Db.DbBook dbBook=new Db("").new DbBook();
-	   Book objBook=new Book( "title", "author list", "1234567892", "mypublisher", "22/09/2008", "available",1);
-               
-	   dbBook.insretBook(objBook);
-       
-        
-	}catch(Exception e)
-	{
-		System.out.println("11122");
-		e.printStackTrace();
 	}
-	
-}
-	
-	
-	
-	public static void testReadBook()
-	{
-		try{
+
+	public static void testUniLibgetStudentRecord2() {
+		Ass02BookSoap ubook = new Ass02BookSoap();
+		ubook.getRecordsStudent2(1);
+
+	}
+
+	public static void testUniLibgetStudentRecord() {
+		Ass02BookSoap ubook = new Ass02BookSoap();
+		ubook.getRecordsStudent(1);
+
+	}
+
+	public static void testUniLibgetAllBook() {
+		Ass02BookSoap ubook = new Ass02BookSoap();
+		ubook.getAllBook();
+
+	}
+
+	public static void testgetLastid() {
+		try {
+
+			Db.DbBook dbBook = new Db("").new DbBook();
+			Integer bookid = dbBook.getLastIdinserted();
+
+			System.out.println(bookid);
+
+		} catch (Exception e) {
+			System.out.println("11122");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void testdeleteBook() {
+		try {
+			System.out.println("##### delete last element#####");
+			Db.DbBook dbBook = new Db("").new DbBook();
+
+			Integer bookid = dbBook.getLastIdinserted();
+			dbBook.deleteBook(bookid);
+
+		} catch (Exception e) {
+			System.out.println("11122");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void testinsertBook() {
+		try {
+			System.out.println("##### insert book#####");
+			Db.DbBook dbBook = new Db("").new DbBook();
+			Book objBook = new Book("title", "author list", "1234567892",
+					"mypublisher", "22/09/2008", "available", 1);
+
+			dbBook.insretBook(objBook);
+
+		} catch (Exception e) {
+			System.out.println("11122");
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void testReadBook() {
+		try {
 			System.out.println("##### list of all book#####");
-		   Db.DbBook dbBook=new Db("").new DbBook();
-	       ResultSet myRs=dbBook.getAllBooks();
-	               
-	       
-	       
-	        while(myRs.next()){
-	            System.out.println("book name:"+myRs.getString("title")+" isbn:"+myRs.getString("isbn"));
-	        }
-		}catch(Exception e)
-		{
+			Db.DbBook dbBook = new Db("").new DbBook();
+			ResultSet myRs = dbBook.getAllBooks();
+
+			while (myRs.next()) {
+				System.out.println("book name:" + myRs.getString("title")
+						+ " isbn:" + myRs.getString("isbn"));
+			}
+		} catch (Exception e) {
 			System.out.println("11122");
 		}
-		
-	}
 
-	
-	
-	   
+	}
 
 }
--- Ass02StudentSoap/src/main/java/testPackage/RunDb.java
+++ Ass02StudentSoap/src/main/java/testPackage/RunDb.java
@@ -1,60 +1,47 @@
 package testPackage;
-import au.edu.swin.waa.*;
-import java.sql.*;
 
+import au.edu.swin.waa.Ass02StudentSoap;
 
 public class RunDb {
-	
-public static void main(String[] args){// ToDO
-	try{
-	//	testGetAllStudent();
-	//	testinsertStudent();
-		testdeleteStudent(1);
-		testisStudentExist(1, "33");
-				
-					        
-	}catch(Exception e)
-	{
-						System.out.println(e.getMessage());
+
+	public static void main(String[] args) {// ToDO
+		try {
+			testGetAllStudent();
+			// testinsertStudent();
+			// testdeleteStudent(1);
+			// testisStudentExist(1, "33");
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
+
+	public static void testisStudentExist(Integer stid, String pin) {
+		Ass02StudentSoap uniStudent = new Ass02StudentSoap();
+		uniStudent.isStudentExist(stid, pin);
 	}
-public static void testisStudentExist(Integer stid,String pin)
-{
-	Ass02StudentSoap uniStudent=new Ass02StudentSoap();
-	uniStudent.isStudentExist(stid, pin);
-}
-public static void testdeleteStudent(Integer stid)
-{
-	try{
-	Ass02StudentSoap uniStudent=new Ass02StudentSoap();
-	uniStudent.deleteStudent(stid);
-	}catch(Exception e)
-	{
-		System.out.println(e.getMessage());
+
+	public static void testdeleteStudent(Integer stid) {
+		try {
+			Ass02StudentSoap uniStudent = new Ass02StudentSoap();
+			uniStudent.deleteStudent(stid);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
-}
 
-public static void testGetAllStudent()
-{
-	Ass02StudentSoap uniStudent=new Ass02StudentSoap();
-	uniStudent.getAllStudent();
-}
-
-public static void testinsertStudent()
-{
-	try{
-	Ass02StudentSoap uniStudent=new Ass02StudentSoap();
-	uniStudent.addStudent("mansour", "3345");
-	}catch(Exception e)
-	{
-		System.out.println(e.getMessage());
+	public static void testGetAllStudent() {
+		Ass02StudentSoap uniStudent = new Ass02StudentSoap();
+		uniStudent.getAllStudent();
 	}
-}
 
-
-
-	
-	
-	   
+	public static void testinsertStudent() {
+		try {
+			Ass02StudentSoap uniStudent = new Ass02StudentSoap();
+			uniStudent.addStudent("mansour", "3345");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
 }
