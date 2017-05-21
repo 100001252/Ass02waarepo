5e3e7279f5ed250e032cd58203bb92b9843558a7
mansour.sh <100001252@student.swin.edu.au>
2017-05-21 14:53:49 +1000

    new service book info system like a book shop

---
+++ Ass02BookInformation/src/main/java/au/sw/BookException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.sw;
+
+/** that is an bookException class to return any exception related to book db or book class
+ * with all common functions
+ *
+ * @author XC8184
+ */
+public class BookException extends Exception {
+    
+       public BookException(String mess) { super(mess); }
+  public BookException(String message, Throwable cause) { super(message, cause); }
+  public BookException(Throwable cause) { super(cause); }
+
+    
+}
